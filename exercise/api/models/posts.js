54d0e0636ead66b8cc1f9f965ef7c1f7a7963568
Tomek <t.wolyniec@tlen.pl>
2020-10-08 11:19:39 +0200

    Created API Endpoint

--- exercise/api/models/posts.js
+++ exercise/api/models/posts.js
@@ -1,22 +1,34 @@
+const fs = require("fs");
 
-const fs = require('fs');
-
-const PATH = './data.json';
+const PATH = "./data.json";
 
 class Post {
     get() {
         return this.readData();
     }
 
-    getIndividualBlog() {}
+    getIndividualBlog(postId) {
+        const posts = this.readData()
+        const foundPost = posts.find(({id}) => id == postId)
+        return foundPost
+    }
 
-    add() {}
+    add(newPost) {
+        const data = this.readData()
+        data.unshift(newPost)
+        this.storeData(data)
+    }
 
-    readData(){
+    readData() {
         const rawData = fs.readFileSync(PATH);
         const data = JSON.parse(rawData);
         return data;
     }
+
+    storeData(rawData) {
+        const data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
+    }
 }
 
-module.exports = Post
+module.exports = Post;
--- exercise/app.js
+++ exercise/app.js
@@ -1,15 +1,19 @@
-
-
 const express = require("express");
 const app = express();
 
 const Post = require("./api/models/posts");
 const postData = new Post();
 
-const data = require('./data.json');
-
 app.get("/", (req, res) => {
     res.status(200).send(postData.get());
+});
+
+app.get("/post/:post_id", (req, res) => {
+    const postId = req.params.post_id;
+    const foundPost = postData.getIndividualBlog(postId)
+    if (foundPost){
+        res.status(200).send(foundPost);
+    } else res.status(404).send('Not found')
 });
 
 app.listen(3000, () => console.log("Nasłuchuję localhost:3000"));

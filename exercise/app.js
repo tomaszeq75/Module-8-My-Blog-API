6ae8342a929acffc325c592ec900aad302970b77
Tomasz Wołyniec <t.wolyniec@tlen.pl>
2020-11-13 11:05:30 +0100

    13,11

--- exercise/app.js
+++ exercise/app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 const Post = require("./api/models/posts");
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const postData = new Post();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
 })
 
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static('uploads'));
 
 // get all posts
 app.get("/api/posts", (req, res) => {
@@ -18,10 +22,22 @@ app.get("/api/posts", (req, res) => {
 // get individual post
 app.get("/api/posts/:post_id", (req, res) => {
     const postId = req.params.post_id;
-    const foundPost = postData.getIndividualBlog(postId)
-    if (foundPost){
+    const foundPost = postData.getIndividualBlog(postId);
+    if (foundPost) {
         res.status(200).send(foundPost);
-    } else res.status(404).send('Not found')
+    } else res.status(404).send('Not found');
 });
+
+app.post("/api/posts", upload.single('post_image'), (req, res) => {
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file,
+        "added_date": `${Date.now()}`
+    }
+    console.log(req.body);
+    res.status(201).send(newPost);
+})
 
 app.listen(3000, () => console.log("Nasłuchuję localhost:3000"));
